58da096f1fb453dadffde298729b340ea2b27098
Aerius01 <david.james.dj551@gmail.com>
2021-05-10 17:38:52 +0200

    Added incomplete in-client Unity button for file upload

---
+++ Assets/Scripts/BrowserFileLoadingDialog.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Runtime.InteropServices;
+public class BrowserFileLoadingDialog : MonoBehaviour
+{
+    public Text UrlTextField;
+
+    [DllImport("__Internal")] private static extern void AddClickListenerForFileDialog();
+
+    void Start()
+    {
+        AddClickListenerForFileDialog();
+    }
+
+    public void FileDialogResult(string fileUrl)
+    {
+        Debug.Log(fileUrl);
+        UrlTextField.text = fileUrl;
+        StartCoroutine(LoadBlob(fileUrl));
+    }
+
+    IEnumerator LoadBlob(string url)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(url);
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result != UnityWebRequest.Result.ConnectionError && webRequest.result != UnityWebRequest.Result.ProtocolError)
+        {
+        // Get text content like this:
+        Debug.Log(webRequest.downloadHandler.text);
+
+        }
+    }
+}
